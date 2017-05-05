5baaf1b8865789e554cec6feec6f908d0fd4c92e
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-05-05 10:37:17 -0400

    main page button navigation fix
    Former-commit-id: 497392e38f8a411ea4b0739cdd0c2a9570764cc2

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import {ListPage} from '../list/list';
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  listPage = ListPage;
   constructor() {
-
+    console.log("test");
   }
 }
