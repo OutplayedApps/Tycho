480d80e550d3393466596db5e951ae3a98bed01c
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-05-06 18:23:28 -0400

    bio study guide
    Former-commit-id: 7b219a85c3c668f5d0706248ab0d2917c65e0e65

--- src/pages/item-details/item-details.ts
+++ src/pages/item-details/item-details.ts
@@ -11,21 +11,22 @@ import { NavController, NavParams, LoadingController } from 'ionic-angular';
   templateUrl: 'item-details.html'
 })
 export class ItemDetailsPage {
-  Class: any;
-  selectedItem: any;
-  pageContent: any;
+  Class:any;
+  selectedItem:any;
+  pageContent:any;
 
-  loading: any;
+  loading:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController,
-              private http: Http) {
+  constructor(public navCtrl:NavController, public navParams:NavParams, public loadingCtrl:LoadingController,
+              private http:Http) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
     console.log(this.selectedItem);
   }
+
   ionViewDidLoad() {
 
-    //this.presentLoadingCustom();
+    this.presentLoadingCustom();
     var url = '//cdn.mozilla.net/pdfjs/tracemonkey.pdf';
     url = 'assets/files/YGKschoolsofthought.pdf'
     console.log(this.selectedItem);
@@ -42,7 +43,9 @@ export class ItemDetailsPage {
         }
         return div.innerHTML;
       }
+
       //data = stripScripts(data);
+      (<any>window).loading.dismiss();
       this.pageContent = data;
     });
 
@@ -50,33 +53,32 @@ export class ItemDetailsPage {
 
 
   }
+
   getFile() {
     var url = '';
     url = 'https://docs.google.com/document/d/1K7rh9iKomflEKEs5ApstHQM9vvNV73UIY8j3LA_rcpU/pub';
-    url = 'https://www.googleapis.com/drive/v3/files/fileId/export?fileId=1K7rh9iKomflEKEs5ApstHQM9vvNV73UIY8j3LA_rcpU&mimeType=text/html&key=AIzaSyDIzqeDtau8sP6OtsURRSsSjUxDR1Qd1RA';
+    url = 'https://www.googleapis.com/drive/v3/files/fileId/export?fileId=1Xxqt2V9gu0ghEMBgVm4xNbEO579Pq8uNXbc5r1OD58I&mimeType=text/html&key=AIzaSyDIzqeDtau8sP6OtsURRSsSjUxDR1Qd1RA';
     return this.http.get(url)
       .map((res:Response) => res.text());
   }
 
-  }
-/*
- presentLoadingCustom() {
-  let loading = this.loadingCtrl.create({
-    spinner: 'hide',
-    content: `
+
+  presentLoadingCustom() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: `
       <div class="loading-custom-spinner-container">
         <div class="loading-custom-spinner-box"></div>
         <div class="loading-content">Loading file...</div>
       </div>`
-  });
+    });
 
-  loading.onDidDismiss(() => {
-    console.log('Dismissed loading');
-  });
+    loading.onDidDismiss(() => {
+      console.log('Dismissed loading');
+    });
 
-  loading.present();
+    loading.present();
     (<any>window).loading = loading;
+  }
+
 }
-
-
-*/
