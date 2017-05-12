067d22318f70e8c138e06934558211f8f1c6facd
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-05-12 14:23:08 -0400

    splashscreen fix
    Former-commit-id: 9e5a179ef204fc1e032176b8537151f016cff2c7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      setTimeout(() => this.splashScreen.hide(), 100);
     });
   }
 
