fb1496fa2a1fb8e7682edf1e32344d114c4ac0d9
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-06-15 16:53:54 -0400

    packet list stuff, also gave up on using cordova file and am using a custom json hook page instead
    Former-commit-id: 8e26493e9f38be209c7b1618de879e71ff8b3758
    Former-commit-id: a4c1ef225cd77fe272eded5ccb46484ed1b266cd

---
+++ src/assets/packets/createjson.js
@@ -0,0 +1,10 @@
+const dirTree = require('directory-tree');
+var fs = require('fs');
+const tree = dirTree(__dirname);
+fs.writeFile("./filestructure.json", JSON.stringify(tree), function(err) {
+  if(err) {
+    return console.log(err);
+  }
+
+  console.log("The file was saved!");
+});
--- src/pages/packet-list/packet-list.ts
+++ src/pages/packet-list/packet-list.ts
@@ -20,12 +20,15 @@ export class PacketListPage {
   type: Number;
   path: String;
   listTitle: String;
+  currentSet: String;
+  currentLevel: String;
   items: any;
   TYPES: any = {
-  DIFFICULTYLIST: 1,
-  SETLIST: 2,
-  PACKETLIST: 3
+    DIFFICULTYLIST: 1,
+    SETLIST: 2,
+    PACKETLIST: 3
   };
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public apiService: ApiService,
               private file: File) {
@@ -34,28 +37,72 @@ export class PacketListPage {
     if (!this.type) this.type = this.TYPES.DIFFICULTYLIST;
     //console.log(this.type);
     this.path = navParams.get('path');
+    this.currentLevel = navParams.get('currentLevel');
+    this.currentSet = decodeURIComponent(navParams.get('currentSet'));
+    if (this.currentLevel == "undefined" || this.currentLevel == "null") this.currentLevel = null;
+    if (this.currentSet == "undefined" || this.currentSet == "null") this.currentSet = null;
+    if (this.currentLevel) {this.type = this.TYPES.SETLIST;}
+    if (this.currentSet) {this.type = this.TYPES.PACKETLIST;}
   }
 
   ionViewDidLoad() {
     var items, listTitle;
     (<any>window).file = this.file;
+    (<any>window).This = this;
     this.file.checkDir(this.file.dataDirectory, '')
       .then((files) => console.log('Directory exists'))
       .catch((err) => console.log('Directory doesnt exist'));
+    this.apiService.presentLoadingCustom();
     switch (this.type) {
       case this.TYPES.DIFFICULTYLIST:
         items = [
-          {"title": "Middle School", "icon": "md-book", "description": "Middle school packet archive.\n\nExample: CMST"},
-          {"title": "High School", "icon": "md-laptop", "description":"High school packet archive.\n\nExample: LIST, FKT, PACE NSC"},
-          {"title": "Collegiate ", "icon": "md-school", "description":"Collegiate and open packets.\n\nExample: MUT, ACF Fall, ACF Nationals, Chicago Open"}
+          {"title": "Middle School", "icon": "md-book", "description": "Middle school packet archive.\n\nExample: CMST",
+           "currentLevel": "ms"},
+          {"title": "High School", "icon": "md-laptop", "description":"High school packet archive.\n\nExample: LIST, FKT, PACE NSC",
+            "currentLevel": "hs"},
+          {"title": "Collegiate ", "icon": "md-school", "description":"Collegiate and open packets.\n\nExample: MUT, ACF Fall, ACF Nationals, Chicago Open",
+          "currentLevel": "collegiate"}
         ];
         listTitle = "Choose a packet difficulty";
+        (<any>window).loading.dismiss();
         break;
       case this.TYPES.SETLIST:
 
+        this.file.listDir(this.file.applicationDirectory,"www/assets/packets/"+this.currentLevel+"/").then((data) => {
+          console.log(data);
+          items = [];
+          for (let i = 0; i < data.length; i++) {
+            var newItem = {};
+            newItem["currentLevel"] = this.currentLevel;
+            newItem["currentSet"] = data[i].name;
+            newItem["name"] = data[i].name;
+            items.push(newItem);
+          }
+          console.log(items);
+          this.items = items;
+          (<any>window).loading.dismiss();
+        }).catch((err) => {
+          console.error(err);
+        });
         break;
       case this.TYPES.PACKETLIST:
-
+        this.file.listDir(this.file.applicationDirectory,"www/assets/packets/"+this.currentLevel+"/"+this.currentSet+"/").then((data) => {
+          console.log(data);
+          items = [];
+          for (let i = 0; i < data.length; i++) {
+            var newItem = {};
+            newItem["currentLevel"] = this.currentLevel;
+            newItem["currentSet"] = this.currentSet;
+            newItem["currentPacket"] = data[i].name;
+            newItem["name"] = data[i].name;
+            items.push(newItem);
+          }
+          console.log(items);
+          this.items = items;
+          (<any>window).loading.dismiss();
+        }).catch((err) => {
+          console.error(err);
+        });
         break;
     }
     this.items = items;
@@ -65,14 +112,11 @@ export class PacketListPage {
   itemTapped(event, item) {
     var queryParams = {
       item: item,
-      type: 'cat',
-      catName: '',
-      subcatName: '',
-      guideName: '',
-      subcatId: '',
-      catId: ''
+      currentLevel: item.currentLevel || null,
+      currentSet: item.currentSet || null,
+      currentPacket: item.currentPacket || null
     };
-    if (item.guideId) {
+    if (item.currentPacket) {
       this.navCtrl.push(PacketDetails, queryParams);
       // opens the study guide itself.
     }
