255fe3702f8bbdc896a249dbb51d369e5e2f76bc
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-06-16 15:25:07 -0400

    ps2pdf script commit
    Former-commit-id: 5294af3cb1980e797c59104d514a4544b9d87716
    Former-commit-id: b5970e1f438c9458a304b07df387209a1a7700ee

--- src/assets/packets/ps2pdfscript.sh
+++ src/assets/packets/ps2pdfscript.sh
@@ -1,5 +1,11 @@
-mkdir new
-for x in *.pdf; do
-echo $x;
-ps2pdf "$x" "new/$x"
+find . -iname "HFT XI Round 2 FINISHED.pdf" | while read x
+do
+	filename="${x##*/}"
+	directory=`dirname "$x"`
+	newDir="./new"${directory#.}
+	mkdir -p "$newDir"
+	newPath="$newDir""/""$filename"
+	echo $x, $newPath
+	ps2pdf "$x" "$newPath"
+	#./pdfsizeopt/pdfsizeopt --use-jbig2=no "$x" "$newPath"
 done
