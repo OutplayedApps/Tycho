e3d52a8c550fbe6c295374fbc81433e2fbf9d171
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-06-16 09:08:13 -0400

    converting docx to pdf
    Former-commit-id: 798d0926899068ad1de2494456cee3d5d81a8a42
    Former-commit-id: 2ce10ffe6caad464f06d314cd030d9cc01fa70ff

---
+++ src/assets/packets/doc2pdf2.ps1
@@ -0,0 +1,20 @@
+$Folders = Get-ChildItem 'C:\Users\arama\git\tycho2\tycho\src\assets\packets\collegiate' -Directory
+
+ForEach ($Folder in $Folders)
+{
+    $wdFormatPDF = 17
+    $word = New-Object -ComObject word.application
+    $word.visible = $false
+    $folderpath = "$($Folder.FullName)\*"
+    $fileTypes = "*.docx","*.doc","*.rtf"
+    Get-ChildItem -path $folderpath -include $fileTypes |
+    foreach-object `
+    {
+     $path =  ($_.fullname).substring(0,($_.FullName).lastindexOf("."))
+     "Converting $path to pdf ..."
+     $doc = $word.documents.open($_.fullname)
+     $doc.saveas([ref] $path, [ref]$wdFormatPDF)
+     $doc.close()
+    }
+    $word.Quit()
+}
