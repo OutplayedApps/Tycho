9374c8018c650dd82ddd20c45e2ade922c9ac35c
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-06-16 15:35:26 -0400

    update script to shrink pdfs twice
    Former-commit-id: 092a78a9949c03249b422b79081bb6b51587a317
    Former-commit-id: 2a94e6e2d9c9110e7b8c518b68a1fb63fbe7cc10

--- src/assets/packets/ps2pdfscript.sh
+++ src/assets/packets/ps2pdfscript.sh
@@ -1,11 +1,20 @@
-find . -iname "HFT XI Round 2 FINISHED.pdf" | while read x
+find . -maxdepth 3 -iname "HFT XI Round 2 FINISHED.pdf" | while read x
 do
 	filename="${x##*/}"
 	directory=`dirname "$x"`
 	newDir="./new"${directory#.}
 	mkdir -p "$newDir"
 	newPath="$newDir""/""$filename"
-	echo $x, $newPath
+	#first optimization:
 	ps2pdf "$x" "$newPath"
-	#./pdfsizeopt/pdfsizeopt --use-jbig2=no "$x" "$newPath"
+	size1=$(wc -c < "$x")
+	size2=$(wc -c < "$newPath")
+	echo $size1" versus "$size2,$x
+	if [ $size2 -gt $size1 ]
+		then
+		cp "$x" "$newDir"
+		echo "\tnow we're replacing it..."
+	fi
+	#second optimization:
+	./pdfsizeopt/pdfsizeopt --use-jbig2=no --use-pngout=no  --do-optimize-images=no "$newPath" "$newPath"
 done
