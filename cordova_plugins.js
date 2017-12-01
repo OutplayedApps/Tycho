1ed949a4246dbc2b4c279be3ad36f93bc66bc72c
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-12-01 14:27:35 -0800

    init deployment

---
+++ cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-app-version/www/AppVersionPlugin.js",
+        "id": "cordova-plugin-app-version.AppVersionPlugin",
+        "pluginId": "cordova-plugin-app-version",
+        "clobbers": [
+            "cordova.getAppVersion"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-market/www/market.js",
+        "id": "cordova-plugin-market.Market",
+        "pluginId": "cordova-plugin-market",
+        "clobbers": [
+            "cordova.plugins.market"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-app-version": "0.1.9",
+    "cordova-plugin-market": "1.2.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ plugins/cordova-plugin-market/www/market.js
@@ -0,0 +1,23 @@
+cordova.define("cordova-plugin-market.Market", function(require, exports, module) { /**
+ * Cordova Market plugin
+ * Author: Miguel Revetria <miguel@xmartlabs.com>
+ * License: Apache 2.0
+ */
+var exec = require('cordova/exec');
+
+function Market() { }
+
+Market.prototype.open = function(appId, callbackContext) {
+    callbackContext = callbackContext || { };
+    exec(callbackContext.success || null, callbackContext.error || null, 'Market', 'open', [appId]);
+};
+
+Market.prototype.search = function(key, callbackContext) {
+    callbackContext = callbackContext || { };
+    exec(callbackContext.success || null, callbackContext.error || null, 'Market', 'search', [key]);
+};
+
+var market = new Market();
+module.exports = market;
+
+});
